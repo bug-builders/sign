e1caba1d186ce3a14897ded044d68542720b50fa
agix <florian.gaultier@gmail.com>
2018-05-26 19:41:27 +0200

    Initial commit

---
+++ sign.js
@@ -0,0 +1,48 @@
+const crypto = require('crypto');
+const forge = require('node-forge');
+const fs = require('fs');
+const cp = require('child_process');
+const fetch = require('node-fetch');
+
+function asciiToHexString(str) {
+  return str
+    .split('')
+    .map(c => `0${c.charCodeAt(0).toString(16)}`.slice(-2))
+    .join('');
+}
+
+function hexStringToAscii(hexx) {
+  const hex = hexx.toString();
+  let str = '';
+  for (let i = 0; i < hex.length; i += 2) {
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  }
+  return str;
+}
+
+function sign(datas, key) {
+  const pss = forge.pss.create({
+    md: forge.md.sha256.create(),
+    mgf: forge.mgf.mgf1.create(forge.md.sha256.create()),
+    saltLength: 32,
+  });
+
+  const md = forge.md.sha256.create();
+  md.update(datas, 'utf8');
+
+  const signature = key.sign(md, pss);
+
+  return asciiToHexString(signature);
+}
+
+const filename = process.argv[2];
+const pkeyFile = process.argv[3] ? process.argv[3] : `${process.env['HOME']}/.ssh/id_rsa`;
+
+const pkey = fs.readFileSync(pkeyFile, 'utf8');
+const privateKey = forge.pki.privateKeyFromPem(pkey);
+// const privateKey = forge.pki.decryptRsaPrivateKey(pem, 'password');
+
+const sha = cp.execFileSync('git', ['hash-object', filename]).toString().trim();
+
+console.log(sign(sha, privateKey));
+
